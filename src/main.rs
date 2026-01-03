7d7240bf0542f50c5899c247a08ad5a13abd0b7f
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-01-03 16:47:09 +0700

    Plan out components

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
-use hack_club_space_program::plugins::demo::DemoPlugin;
+use hack_club_space_program::plugins::game::GamePlugin;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, DemoPlugin)).run();
+    App::new().add_plugins((DefaultPlugins, GamePlugin)).run();
 }
