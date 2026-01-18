beae053b1fb22ce9fec3a65397461733aa87f0aa
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-01-19 00:39:00 +0700

    Enable backtrace by default for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use bevy::prelude::*;
 use hack_club_space_program::plugins::game::{GameLogicPlugin, GameSetupPlugin};
 
+fn enable_backtrace() {
+    const BACKTRACE_KEY: &str = "RUST_BACKTRACE";
+    unsafe {
+        if std::env::var(BACKTRACE_KEY).is_err() {
+            std::env::set_var(BACKTRACE_KEY, "1");
+        }
+    }
+}
+
 fn main() {
+    enable_backtrace();
+
     App::new()
         .add_plugins((DefaultPlugins, GameLogicPlugin, GameSetupPlugin))
         .run();
