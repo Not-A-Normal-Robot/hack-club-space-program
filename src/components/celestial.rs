948a6b53cc50c46dc1810350282953db72dcbcee
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-02-01 23:05:20 +0700

    Improve docs on heightmap

--- src/components/celestial.rs
+++ src/components/celestial.rs
@@ -1,8 +1,30 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::RigidBody;
 
+/// The heightmap of a celestial body.
+///
+/// The heightmap is used for determining the terrain
+/// offsets of a celestial body.
+///
+/// An empty ([]) heightmap means there is no
+/// additional collider on top of the regular ball
+/// collider.
+///
+/// Entries in the array indicate an offset to the
+/// height. This means that if a celestial body is
+/// 6 000 units wide and you want to have a 2.0-unit
+/// tall mountain somewhere, you set the heightmap
+/// at that location to 2.0, not 6002.
+///
+/// TODO: Finer details for heightmaps determined using seeded Perlin noise
 #[derive(Clone, Component, Debug, Default)]
 pub struct Heightmap(pub Box<[f32]>);
+
+impl Heightmap {
+    pub fn empty() -> Self {
+        Self(Box::from([]))
+    }
+}
 
 #[derive(Clone, Copy, Component)]
 #[require(Heightmap)]
