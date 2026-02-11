bfeb93075d200c35f77744c91e8e94b209d49563
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-02-11 16:49:42 +0700

    Work on LoD index getter

--- src/systems/terrain.rs
+++ src/systems/terrain.rs
@@ -8,7 +8,7 @@ use core::{f64::consts::TAU, num::NonZeroU8};
 use fastnoise_lite::{FastNoiseLite, FractalType};
 
 // Math based off a sketch:
-// https://www.desmos.com/calculator/vgdk9qd2ux
+// https://www.desmos.com/calculator/sgyaomwmk6
 
 /// How many vertices for each LoD level.
 pub const LOD_VERTS: usize = 128;
@@ -47,23 +47,40 @@ impl LodVectors {
         prev_focus: f64,
         new_focus: f64,
     ) {
-        debug_assert!(self.0.is_empty());
+        debug_assert!(!self.0.is_empty());
 
-        (0..=ending_level.get()).for_each(|level| {
-            let level_not_loaded = self.0.len() >= level as usize;
-            let lod_needs_updating =
-                lod_level_start(level, prev_focus) != lod_level_start(level, new_focus);
-
-            if level_not_loaded || lod_needs_updating {
-                let vecs = terrain_gen.gen_lod(level, new_focus);
-
-                if level_not_loaded {
-                    self.0.push(vecs);
-                } else {
-                    *self.0.get_mut(level as usize).unwrap() = vecs;
-                }
-            }
+        (1..=ending_level.get()).for_each(|level| {
+            self.update_lod(
+                terrain_gen,
+                NonZeroU8::new(level).unwrap(),
+                prev_focus,
+                new_focus,
+            );
         })
+    }
+
+    fn update_lod(
+        &mut self,
+        terrain_gen: &TerrainGen,
+        level: NonZeroU8,
+        prev_focus: f64,
+        new_focus: f64,
+    ) {
+        let level = level.get();
+        let level_not_loaded = self.0.len() >= level as usize;
+        let lod_needs_updating = NonZeroU8::new(level)
+            .map(|level| lod_level_start(level, prev_focus) != lod_level_start(level, new_focus))
+            .unwrap_or(false);
+
+        if level_not_loaded || lod_needs_updating {
+            let vecs = terrain_gen.gen_lod(level, new_focus);
+
+            if level_not_loaded {
+                self.0.push(vecs);
+            } else {
+                *self.0.get_mut(level as usize).unwrap() = vecs;
+            }
+        }
     }
 
     /// Stitches together this LoD vectors into a mesh.
@@ -112,10 +129,12 @@ impl TerrainGen {
 
     /// Gets the LoD vector array at a certain LoD level.
     fn gen_lod(&self, lod_level: u8, focus: f64) -> [RelativeVector; LOD_VERTS] {
-        // https://www.desmos.com/calculator/vgdk9qd2ux
+        // From Desmos graph:
         // point((tau / verts) (i ⋅ iter_scale + start))
 
-        let start = lod_level_start(lod_level, focus);
+        let start = NonZeroU8::new(lod_level)
+            .map(|l| lod_level_start(l, focus))
+            .unwrap_or_default();
         let iter_scale = (LOD_DIVISIONS as f64).powi(-(lod_level as i32));
 
         core::array::from_fn(|i| {
@@ -145,8 +164,8 @@ struct GlobalData {
 /// Gets the starting theta for a given LoD level and focus theta.
 ///
 /// This is in revolutions. To get the theta in radians, multiply this by tau.
-fn lod_level_start(lod_level: u8, focus: f64) -> f64 {
-    // https://www.desmos.com/calculator/vgdk9qd2ux
+fn lod_level_start(lod_level: NonZeroU8, focus: f64) -> f64 {
+    // From Desmos graph:
     // start = divisions^(1 - level) ⋅
     //  round( (verts / 2pi) ⋅ divisions^(level - 1) ⋅ focus - verts/(2 ⋅ divisions))
     //
@@ -157,12 +176,30 @@ fn lod_level_start(lod_level: u8, focus: f64) -> f64 {
 
     const FRAC_OFFSET: f64 = LOD_VERTS as f64 / (-2.0 * LOD_DIVISIONS as f64);
 
-    let coeff = (LOD_DIVISIONS as f64).powi(1 - lod_level as i32);
+    let coeff = (LOD_DIVISIONS as f64).powi(1 - lod_level.get() as i32);
     let frac = const { LOD_VERTS as f64 / TAU }
-        * (LOD_DIVISIONS as f64).powi(lod_level as i32 - 1)
+        * LOD_DIVISIONS.pow(lod_level.get().wrapping_sub(1) as u32) as f64
         * focus;
 
     coeff * (frac + FRAC_OFFSET).round()
+}
+
+/// Gets the starting vertex index for a given LoD level relative to the previous LoD level.
+///
+/// This is for the part where the LoDs get stitched into a mesh.
+/// This helps figure out where the stitches should be.
+fn lod_level_index(lod_level: NonZeroU8, focus: f64) -> usize {
+    // From Desmos graph:
+    // indices = ((cur_start - prev_start) / prev_iter_scale).rem_euclid(verts)
+    // prev_iter_scale = divisions^(1-o)
+
+    let prev_iter_scale = (LOD_DIVISIONS as f64).powi(1 - (lod_level.get() as i32));
+    let cur_start = lod_level_start(lod_level, focus);
+    let prev_start = NonZeroU8::new(lod_level.get() - 1)
+        .map(|level| lod_level_start(level, focus))
+        .unwrap_or_default();
+
+    ((cur_start - prev_start) / prev_iter_scale).rem_euclid(LOD_VERTS as f64) as usize
 }
 
 #[cfg(test)]
@@ -172,7 +209,7 @@ mod tests {
     use crate::{
         components::celestial::Terrain,
         systems::terrain::{
-            LOD_DIVISIONS, LOD_VERTS, LodVectors, PrevFocus, RelativeVector, TerrainGen,
+            LOD_DIVISIONS, LOD_VERTS, LodVectors, RelativeVector, TerrainGen, lod_level_index,
         },
     };
 
@@ -234,12 +271,55 @@ mod tests {
         const TOLERANCE: f64 = 1e-6;
 
         for level in 0..=TEST_TERRAIN.subdivs {
-            let range = get_range(terrain_gen.gen_lod(level as u8, 0.0));
+            let range = get_range(terrain_gen.gen_lod(level, 0.0));
             let expected_range = FULL_RANGE * (LOD_DIVISIONS as f64).powi(-(level as i32));
             assert!(
                 (range - expected_range).abs() < TOLERANCE,
                 "Expected range of {expected_range}, got {range}"
             );
+        }
+    }
+
+    #[test]
+    fn lod_index() {
+        const FOCUS: f64 = 1.0;
+
+        let indices = (1..=TEST_TERRAIN.subdivs)
+            .map(|level| {
+                let level = NonZeroU8::new(level).unwrap();
+                lod_level_index(level, FOCUS)
+            })
+            .collect::<Box<_>>();
+
+        assert_eq!(indices, Box::from([12, 59, 56, 54, 59, 56, 58, 54]));
+    }
+
+    #[test]
+    #[ignore]
+    fn print_results() {
+        const FOCUS: f64 = 1.0;
+
+        println!("=== Vertices ===");
+        println!("lod,x,y");
+        let terrain_gen = TerrainGen::new(TEST_TERRAIN);
+        let vecs = LodVectors::new_full(
+            &terrain_gen,
+            NonZeroU8::new(TEST_TERRAIN.subdivs).unwrap(),
+            FOCUS,
+        );
+        vecs.0.iter().enumerate().for_each(|(lod_level, vecs)| {
+            vecs.iter().for_each(|vec| {
+                println!("{lod_level},{},{}", vec.0.x, vec.0.y);
+            })
+        });
+
+        println!("=== Indices ===");
+        println!("lod,num");
+        for level in 1..=TEST_TERRAIN.subdivs {
+            let level = NonZeroU8::new(level).unwrap();
+            let index = lod_level_index(level, FOCUS);
+
+            println!("{level},{index}");
         }
     }
 }
