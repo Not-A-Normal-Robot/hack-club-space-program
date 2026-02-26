889696be4d5e19113607a64aabdea0268e59873a
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-02-26 22:18:02 +0700

    Make controls UI use WDXL font

--- src/systems/main_game/ui/controls.rs
+++ src/systems/main_game/ui/controls.rs
@@ -1,4 +1,7 @@
-use crate::resources::{controls::GameControlMode, scene::GameScene};
+use crate::{
+    assets::fonts::URI_FONT_WDXL_LUBRIFONT_SC,
+    resources::{controls::GameControlMode, scene::GameScene},
+};
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -9,11 +12,13 @@ pub fn update_controls_text(
     mut commands: Commands,
     mut text: Query<&mut Text, With<ControlsText>>,
     control_mode: Res<State<GameControlMode>>,
+    server: Res<AssetServer>,
 ) {
     let Some(mut text) = text.iter_mut().next() else {
         commands.spawn((
             Node::DEFAULT,
             Text::new(control_mode.get().to_str()),
+            TextFont::from(server.load(URI_FONT_WDXL_LUBRIFONT_SC)),
             ControlsText,
         ));
         return;
